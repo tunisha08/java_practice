0762c2849f36a02772af54ae78056d884c797b24
tunisha08 <45281592+tunisha08@users.noreply.github.com>
2018-12-03 16:17:42 +0530

    Add files via upload

---
+++ LinkedList.java
@@ -0,0 +1,51 @@
+import java.io.*;
+import java.util.*;
+
+class Node {
+	int data;
+	Node next;
+	Node(int d) {
+        data = d;
+        next = null;
+    }
+}
+
+class Solution {
+
+    public static  Node insert(Node head,int data) {
+        if(head==null){
+            head=new Node(data);
+            return head;
+        }
+        Node n=new Node(data);
+        // new.next=null;
+        Node last=head;
+        while(last.next!=null){
+            last=last.next;
+        }
+        last.next=n;
+        return head;
+
+    }
+
+	public static void display(Node head) {
+        Node start = head;
+        while(start != null) {
+            System.out.print(start.data + " ");
+            start = start.next;
+        }
+    }
+
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        Node head = null;
+        int N = sc.nextInt();
+
+        while(N-- > 0) {
+            int ele = sc.nextInt();
+            head = insert(head,ele);
+        }
+        display(head);
+        sc.close();
+    }
+}
