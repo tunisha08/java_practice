16bae126be5ec46129463c79160afa6aac4e2bed
tunisha08 <45281592+tunisha08@users.noreply.github.com>
2018-12-17 15:38:23 +0530

    Add files via upload

---
+++ regex.java
@@ -0,0 +1,37 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class Solution {
+
+
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        int N = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+        ArrayList<String> a=new ArrayList<String>();
+        for (int NItr = 0; NItr < N; NItr++) {
+            String[] firstNameEmailID = scanner.nextLine().split(" ");
+
+            String firstName = firstNameEmailID[0];
+
+            String emailID = firstNameEmailID[1];
+            if(emailID.contains("@gmail.com")){
+                a.add(firstName);
+            }
+        }
+        Collections.sort(a);
+        for(int i=0;i<a.size();i++){
+            System.out.println(a.get(i));
+        }
+
+
+        scanner.close();
+    }
+}
